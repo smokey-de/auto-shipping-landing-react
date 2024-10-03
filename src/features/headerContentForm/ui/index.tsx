5983c1c0a423d9d7abf44a0f1bb596af08a99ee0
Abrorxon <abrorxon158@gmail.com>
2024-10-03 23:07:44 +0500

    form bug fixed

--- src/features/headerContentForm/ui/index.tsx
+++ src/features/headerContentForm/ui/index.tsx
@@ -1,156 +1,334 @@
-import classes from "./index.module.scss";
+import classes from './index.module.scss';
 import {
   Autocomplete,
   Button,
   Checkbox,
-  Flex,
+  Flex, Input, NumberInput,
   Select,
   TextInput,
-} from "@mantine/core";
-import { CarSvg, FromSvg, ToSvg } from "@/shared/helpers/svg";
-import { useForm } from "@mantine/form";
-import { sendEmail } from "@/shared/helpers/api";
+} from '@mantine/core';
+import { CalendarSvg, CarSvg, EmailSvg, FromSvg, PhoneFormSvg, ToSvg } from '@/shared/helpers/svg';
+import { useForm, UseFormReturnType } from '@mantine/form';
+import { sendEmail } from '@/shared/helpers/api';
 import { useEffect, useState } from 'react';
+import { useDebouncedCallback } from '@mantine/hooks';
+import { Case, Switch } from 'react-if';
+import dayjs from 'dayjs';
+import { IMaskInput } from 'react-imask';
 
-type FormValues = ReturnType<
-  (values: {
-    from: string;
-    vehicleSize: string;
-    to: string;
-    termsOfService: boolean;
-    from_email: string;
-  }) => {
-    from: string;
-    vehicleSize: string;
-    to: string;
-    termsOfService: boolean;
-    from_email: string;
-  }
->;
+const initialValues = {
+  from: '',
+  to: '',
+  vehicle_size: '',
+  from_email: '',
+  terms_of_service: false,
+  vehicle_year: '',
+  vehicle_make: '',
+  vehicle_model: '',
+  pick_up_date: '',
+  phone_number: '',
+};
+
+type FormValues = typeof initialValues
+
+const checkValidLength = (value: string) => value.length > 0 ? null : 'This field is required';
 
 export const HeaderContentForm = () => {
-  const fromsvg = <FromSvg />;
-  const tosvg = <ToSvg />;
-  const carsvg = <CarSvg />;
-  const [autoCompleteData, setAutoCompleteData] = useState<string[]>([]);
+  const [step, setStep] = useState(1);
 
-  useEffect(() => {
-    fetch('https://back.usstartruckingllc.com/api/shipping/zip-codes/').then((res) => {
-      res.json().then((data) => {
-        setAutoCompleteData(data?.results?.map((item: {
-          city: string;
-          state: string;
-          name: string;
-        }) => `${item.city}, ${item.state}, ${item.name}`));
-      });
-    })
-  }, []);
-
-  const form = useForm({
-    mode: "uncontrolled",
-    initialValues: {
-      from: '',
-      to: '',
-      vehicleSize: "",
-      from_email: "",
-      termsOfService: false,
-    },
+  const form = useForm<FormValues>({
+    mode: 'uncontrolled',
+    initialValues,
 
     validate: {
-      from_email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
-      from: (value) => (value.length > 0 ? null : "This field is required"),
-      to: (value) => (value.length > 0 ? null : "This field is required"),
-      vehicleSize: (value) =>
-        value.length > 0 ? null : "This field is required",
+      terms_of_service: (value) =>
+        value ? null : 'If you would like to continue, Please agree to our terms in the above!\n',
+      from_email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+      from: checkValidLength,
+      to: checkValidLength,
+      vehicle_size: checkValidLength,
+      vehicle_year: checkValidLength,
+      vehicle_make: checkValidLength,
+      vehicle_model: checkValidLength,
+      pick_up_date: checkValidLength,
     },
   });
 
   const handleSubmit = (values: FormValues) => {
     sendEmail(values, (type) => {
-      if (type === "success") form.reset();
+      if (type === 'success') {
+        form.reset();
+        setStep(1);
+      }
     });
   };
+
 
   return (
     <div className={classes.formWrapper}>
       <h2 className={classes.formTitle}>
         GET A FREE INSTANT QUOTE or CALL NOW
         <br />
-        <div className={"flex items-center gap-3"}>
+        <div className={'flex items-center gap-3'}>
           <a href="tel: +1(917) 627-5498">(917) 627-5498</a>
           <a href="tel: +1(865) 776-9121">(865) 776-9121</a>
         </div>
       </h2>
       <hr className={classes.formTitleHr} />
 
-      <form
-        onSubmit={form.onSubmit((values) => {
-          handleSubmit(values);
-        })}
+      <Flex
+        direction={'column' as React.CSSProperties['flexDirection']}
+        gap={'1rem'}
       >
-        <Flex
-          direction={"column" as React.CSSProperties["flexDirection"]}
-          gap={"1rem"}
-        >
-          <Autocomplete
-            leftSectionPointerEvents="none"
-            leftSection={fromsvg}
-            size="lg"
-            placeholder="From (Zip code or City)"
-            data={autoCompleteData}
-            variant="filled"
-            radius="md"
-            key={form.key("from")}
-            {...form.getInputProps("from")}
-          />
 
-          <Autocomplete
-            leftSectionPointerEvents="none"
-            leftSection={tosvg}
-            size="lg"
-            placeholder="To (Zip code or City)"
-            data={autoCompleteData}
-            variant="filled"
-            radius="md"
-            key={form.key("to")}
-            {...form.getInputProps("to")}
-          />
+        <Switch>
+          <Case condition={step === 1}>
+            <FirstStep form={form} />
+          </Case>
+          <Case condition={step === 2}>
+            <SecondStep form={form} />
+          </Case>
+          <Case condition={step === 3}>
+            <LastStep form={form} />
+          </Case>
+        </Switch>
 
-          <Select
-            leftSectionPointerEvents="none"
-            leftSection={carsvg}
-            size="lg"
-            searchable
-            placeholder="Vehicle size"
-            checkIconPosition="right"
-            data={["Car", "SUV", "Pickup", "Motorcycle"]}
-            variant="filled"
-            radius="md"
-            key={form.key("vehicleSize")}
-            {...form.getInputProps("vehicleSize")}
-          />
+        <Checkbox
+          label="By submitting this form you agree to receive SMS messages from our company."
+          key={form.key('terms_of_service')}
+          {...form.getInputProps('terms_of_service', { type: 'checkbox' })}
+        />
 
-          <TextInput
-            radius="md"
-            variant="filled"
-            size="lg"
-            withAsterisk
-            placeholder="your@email.com"
-            key={form.key("from_email")}
-            {...form.getInputProps("from_email")}
-          />
+        <Button fullWidth className={classes.formBtn} onClick={() => {
+          if (step === 1) {
+            const from = form.validateField('from');
+            const to = form.validateField('to');
+            const vehicle_size = form.validateField('vehicle_size');
+            const terms_of_service = form.validateField('terms_of_service');
+            if (from.error === null && to.error === null && vehicle_size.error === null) {
+              setStep(2);
+            } else {
+              form.setErrors({
+                from: from.error,
+                to: to.error,
+                vehicle_size: vehicle_size.error,
+                terms_of_service: terms_of_service.error,
+              });
+            }
+          } else if (step === 2) {
+            const vehicle_year = form.validateField('vehicle_year');
+            const vehicle_make = form.validateField('vehicle_make');
+            const vehicle_model = form.validateField('vehicle_model');
+            const terms_of_service = form.validateField('terms_of_service');
 
-          <Checkbox
-            label="By submitting this form you agree to receive SMS messages from our company."
-            key={form.key("termsOfService")}
-            {...form.getInputProps("termsOfService", { type: "checkbox" })}
-          />
-
-          <Button fullWidth className={classes.formBtn} type={"submit"}>
-            Continue
-          </Button>
-        </Flex>
-      </form>
+            if (vehicle_year.error === null && vehicle_make.error === null && vehicle_model.error === null && terms_of_service.error === null) {
+              setStep(3);
+            } else {
+              form.setErrors({
+                vehicle_year: vehicle_year.error,
+                vehicle_make: vehicle_make.error,
+                vehicle_model: vehicle_model.error,
+                terms_of_service: terms_of_service.error,
+              });
+            }
+          } else if (step === 3) {
+            const from_email = form.validateField('from_email');
+            const pick_up_date = form.validateField('pick_up_date');
+            const terms_of_service = form.validateField('terms_of_service');
+            if (from_email.error === null && pick_up_date.error === null && terms_of_service.error === null) {
+              handleSubmit(form.getValues());
+              return;
+            } else {
+              form.setErrors({
+                from_email: from_email.error,
+                pick_up_date: pick_up_date.error,
+                terms_of_service: terms_of_service.error,
+              });
+            }
+          }
+        }}>
+          {step === 3 ? 'Submit' : 'Continue'}
+        </Button>
+      </Flex>
     </div>
+  );
+};
+
+
+function getSearchResults(query: string): Promise<string[]> {
+  return fetch(`https://back.usstartruckingllc.com/api/shipping/zip-codes/?search=${query}`).then((res) =>
+    res.json().then((data) => data.results.map((item: {
+      city: string;
+      state: string;
+      name: string;
+    }) => `${item.city}, ${item.state}, ${item.name}`)),
+  );
+}
+
+const FirstStep = ({ form }: { form: UseFormReturnType<FormValues, any> }) => {
+  const [autoCompleteDataFrom, setAutoCompleteDataFrom] = useState<string[]>([]);
+  const [autoCompleteDataTo, setAutoCompleteDataTo] = useState<string[]>([]);
+  const handleChange = (formKey: keyof FormValues, value: string) => {
+    form.setFieldValue(formKey, value, {
+      forceUpdate: false,
+    });
+    handleSearch(value,formKey);
+  };
+
+  useEffect(() => {
+    handleSearchFetching('', 'from');
+    handleSearchFetching('', 'to');
+  }, []);
+
+  const handleSearch = useDebouncedCallback(async (query: string,formKey: keyof FormValues) => {
+   await handleSearchFetching(query,formKey);
+  }, 500);
+
+  const handleSearchFetching = async (query: string,formKey: keyof FormValues) => {
+    if (formKey === 'from') {
+      const data = await getSearchResults(query);
+      setAutoCompleteDataFrom(data);
+    } else {
+      const data = await getSearchResults(query);
+      setAutoCompleteDataTo(data);
+    }
+  }
+
+  return (
+    <>
+      <Autocomplete
+        leftSectionPointerEvents="none"
+        leftSection={<FromSvg />}
+        size="lg"
+        placeholder="From (Zip code or City)"
+        data={autoCompleteDataFrom}
+        variant="filled"
+        radius="md"
+        key={form.key('from')}
+        {...form.getInputProps('from')}
+        onChange={(value) => handleChange('from', value)}
+      />
+
+      <Autocomplete
+        leftSectionPointerEvents="none"
+        leftSection={<ToSvg />}
+        size="lg"
+        placeholder="To (Zip code or City)"
+        data={autoCompleteDataTo}
+        variant="filled"
+        radius="md"
+        key={form.key('to')}
+        {...form.getInputProps('to')}
+        onChange={(value) => handleChange('to', value)}
+      />
+
+      <Select
+        leftSectionPointerEvents="none"
+        leftSection={<CarSvg />}
+        size="lg"
+        searchable
+        placeholder="Vehicle size"
+        checkIconPosition="right"
+        data={['Car', 'SUV', 'Pickup', 'Motorcycle']}
+        variant="filled"
+        radius="md"
+        key={form.key('vehicle_size')}
+        {...form.getInputProps('vehicle_size')}
+      />
+
+    </>
+  );
+};
+
+const SecondStep = ({ form }: { form: UseFormReturnType<FormValues, any> }) => {
+
+  return (
+    <>
+      <Select
+        leftSectionPointerEvents="none"
+        leftSection={<CalendarSvg />}
+        size="lg"
+        searchable
+        placeholder="Vehicle year"
+        checkIconPosition="right"
+        data={Array.from({ length: 50 }, (_, i) => String(dayjs().year() - i))}
+        variant="filled"
+        radius="md"
+        key={form.key('vehicle_year')}
+        {...form.getInputProps('vehicle_year')}
+      />
+
+      <TextInput
+        leftSectionPointerEvents="none"
+        leftSection={<CarSvg />}
+        radius="md"
+        variant="filled"
+        size="lg"
+        withAsterisk
+        placeholder="Vehicle make"
+        key={form.key('vehicle_make')}
+        {...form.getInputProps('vehicle_make')}
+      />
+
+      <TextInput
+        leftSectionPointerEvents="none"
+        leftSection={<CarSvg />}
+        radius="md"
+        variant="filled"
+        size="lg"
+        withAsterisk
+        placeholder="Vehicle model"
+        key={form.key('vehicle_model')}
+        {...form.getInputProps('vehicle_model')}
+      />
+    </>
+  );
+};
+
+const LastStep = ({ form }: { form: UseFormReturnType<FormValues, any> }) => {
+  return (
+    <>
+      <TextInput
+        leftSectionPointerEvents="none"
+        leftSection={<EmailSvg />}
+        radius="md"
+        variant="filled"
+        size="lg"
+        withAsterisk
+        placeholder="example@email.com"
+        key={form.key('from_email')}
+        {...form.getInputProps('from_email')}
+      />
+
+      <Select
+        leftSectionPointerEvents="none"
+        leftSection={<CarSvg />}
+        size="lg"
+        searchable
+        placeholder="Pick up date"
+        checkIconPosition="right"
+        data={[
+          'As soon as possible',
+          'Within 7 days',
+          'Within 2 weeks',
+          'On a specific date',
+          'On a specific date ',
+        ]} variant="filled"
+        radius="md"
+        key={form.key('pick_up_date')}
+        {...form.getInputProps('pick_up_date')}
+      />
+
+      <Input
+        component={IMaskInput} mask="(000) 000-0000"
+        leftSectionPointerEvents="none"
+        leftSection={<PhoneFormSvg />}
+        variant="filled"
+        size="lg"
+        radius="md"
+        placeholder={'(000) 000-0000'}
+        key={form.key('phone_number')}
+        {...form.getInputProps('phone_number')} />
+    </>
   );
 };
